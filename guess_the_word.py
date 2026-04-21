fd4a73528096c87445d6f4bf6b023b49152a8f77
karim-abud <karimabudayah@gmail.com>
2026-04-21 04:37:47 -0700

    Added general display and guess input

--- guess_the_word.py
+++ guess_the_word.py
@@ -11,3 +11,21 @@ guessed_letters = [];
 remainingattempts = 6;
 
 print("-- HANGMAN V1 (KARIM) --");
+
+while remainingattempts > 0: # If the attempts is above 0
+    
+    display = "";
+    for letter in secretword:
+        if letter in guessed_letters:
+            display += letter + " "; # Added space for clear formatting
+        else:
+            display += "_ "; # Added space for clear formatting
+            
+    print(f"\nComputer-Related Word: {display}");
+    print(f"Attempts left: {remainingattempts}");
+
+    if "_" not in display: # No "_" means all "_" have been replaced
+        print(f"\nCongratulations! You guessed this word: {secretword}");
+        break;
+        
+    guess = input("Guess one letter: ").upper(); # The guess letter itself, with upper() to make sure it's not caps-sensitive
