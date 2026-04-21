3a71ff2933a0be54bace76ce2ae6001cd6e87824
karim-abud <karimabudayah@gmail.com>
2026-04-21 05:58:27 -0700

    Finished .py test file

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -12,5 +12,25 @@ remainingattempts = 6;
 
 print("-- HANGMAN V1 TESTING (KARIM) --"); # Title/Header
 
+# Test to make sure the selected word comes from the list
 if secretword in word_list:
     print(f"1 Passed, {secretword} is in {word_list}");
+else:
+    print(f"1 Failed, {secretword} is not in {word_list}");
+
+# Test to make sure the correct guesses are processed accurately
+testsecretword = "PYTHON";
+guessright = "P";
+
+if guessright in testsecretword:
+    print(f"2 Passed, {guessright} is in {testsecretword}");
+else:
+    print(f"2 Failed, {guessright} is not in {testsecretword}");
+
+# Test to make sure the incorrect guesses are processed accurately
+guesswrong = "X";
+
+if guesswrong not in testsecretword:
+    print(f"3 Passed, {guessright} is accurately not in {testsecretword}");
+else:
+    print(f"3 Failed, {guessright} is detected as in {testsecretword}");
